4a3d5b6f9963665b2c564386ce94de657efa9668
Neill Robson <neill@neillrobson.com>
2026-04-06 15:59:57 -0400

    Finish 48: Self Powers

--- src/Solutions/P0048.hs
+++ src/Solutions/P0048.hs
@@ -1,7 +1,7 @@
 module Solutions.P0048 where
 
 solve :: Integer
-solve = undefined
+solve = selfPowerSum (10 ^ (10 :: Integer)) 1000
 
 --------------------------------------------------------------------------------
 
@@ -19,3 +19,14 @@ modularExp b e m =
       e' = e `div` 2
       c = if odd e then b `mod` m else 1
    in (c * modularExp b' e' m) `mod` m
+
+sumModM :: Modulus -> [Integer] -> Integer
+sumModM m = foldr ((`mod` m) .: (+)) 0
+
+selfPowerSum :: Modulus -> Integer -> Integer
+selfPowerSum m x = sumModM m $ map (\i -> modularExp i i m) [1 .. x]
+
+--------------------------------------------------------------------------------
+
+(.:) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(f .: g) x y = f $ g x y
