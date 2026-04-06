2eb2f0e61696272aa56a8cbda09b1bd6698ec196
Neill Robson <neill@neillrobson.com>
2026-04-06 15:06:34 -0400

    Fix build issues, simplify

---
+++ src/Solutions/P0048.hs
@@ -0,0 +1,10 @@
+module Solutions.P0048 where
+
+solve :: Integer
+solve = undefined
+
+--------------------------------------------------------------------------------
+
+modularExp :: Integer -> Integer -> Integer -> Integer
+modularExp b e m
+  | e == 0 = 1
