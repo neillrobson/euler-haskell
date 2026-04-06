3f65db8f22b74bdc039025626de06471194af22d
Neill Robson <neill@neillrobson.com>
2026-04-06 15:21:39 -0400

    Modular exponentiation

--- src/Solutions/P0048.hs
+++ src/Solutions/P0048.hs
@@ -5,6 +5,17 @@ solve = undefined
 
 --------------------------------------------------------------------------------
 
-modularExp :: Integer -> Integer -> Integer -> Integer
-modularExp b e m
-  | e == 0 = 1
+type Base = Integer
+
+type Exponent = Integer
+
+type Modulus = Integer
+
+modularExp :: Base -> Exponent -> Modulus -> Integer
+modularExp _ _ 1 = 0
+modularExp _ 0 _ = 1
+modularExp b e m =
+  let b' = (b ^ (2 :: Integer)) `mod` m
+      e' = e `div` 2
+      c = if odd e then b `mod` m else 1
+   in (c * modularExp b' e' m) `mod` m
