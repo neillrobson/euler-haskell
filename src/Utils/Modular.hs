4bbe021754cc9bccecff52978285ee38f82fa0d2
Neill Robson <neill@neillrobson.com>
2026-04-13 15:59:31 -0400

    Typesafe mod impl

---
+++ src/Utils/Modular.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE KindSignatures #-}
+
+module Utils.Modular where
+
+import GHC.TypeLits (KnownNat, Nat, natVal)
+
+newtype Mod (m :: Nat) = Mod Integer deriving (Eq, Ord)
+
+instance (KnownNat m) => Num (Mod m) where
+  mx@(Mod x) * Mod y = Mod $ x * y `mod` natVal mx
+  mx@(Mod x) + Mod y = Mod $ if xy > m then xy - m else xy
+    where
+      xy = x + y
+      m = natVal mx
+  negate mx@(Mod x) = Mod $ if x == 0 then 0 else natVal mx - x
+  abs = id
+  signum (Mod x) = Mod $ signum x
+  fromInteger x = mx
+    where
+      mx = Mod $ x `mod` natVal mx
