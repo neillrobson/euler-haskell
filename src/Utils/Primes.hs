58d9d6d76e89ddbf5544adead281772720d1e85c
Neill Robson <neill@neillrobson.com>
2026-04-16 15:01:32 -0400

    Naive but functional prime and phi generation

---
+++ src/Utils/Primes.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TupleSections #-}
+
+module Utils.Primes where
+
+import Data.Int (Int64)
+import GHC.Arr (accum, elems, listArray)
+
+primes :: (Integral a) => [a]
+primes = 2 : 3 : filter isPrime (chain [5, 11 ..] [7, 13 ..])
+  where
+    chain [] ys = ys
+    chain xs [] = xs
+    chain (x : xs) (y : ys) = x : y : chain xs ys
+
+isPrime :: (Integral a) => a -> Bool
+isPrime x = all (\i -> x `mod` i /= 0) $ takeWhile (\i -> i * i <= x) primes
+
+eulerPhi :: Int64 -> [Int64]
+eulerPhi limit = elems $ accum (\x p -> x - x `div` p) phi primePairs
+  where
+    ps = takeWhile (<= limit) primes
+    idxs = map (\p -> takeWhile (<= limit) [p, p * 2 ..]) ps
+    primePairs = concat $ zipWith (\is p -> (,p) <$> is) idxs ps
+    phi = listArray (1, limit) [1 .. limit]
