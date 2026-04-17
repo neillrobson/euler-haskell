77d5fae1ff0a6e339a3e8980a5d59e2e25eaefd3
Neill Robson <neill@neillrobson.com>
2026-04-17 14:34:36 -0400

    Add fast version of 69

--- src/Solutions/P0069.hs
+++ src/Solutions/P0069.hs
@@ -3,15 +3,26 @@ module Solutions.P0069 where
 import Data.Foldable (Foldable (foldl'))
 import Data.Ratio ((%))
 import Data.Word (Word64)
-import Utils.Primes (phiST)
+import Utils.Primes (phiST, primes)
 
+-- Fast smart version.
+-- phi(n) = n * */ (1 - 1/p)
+-- n / phi(n) = 1 / (*/ (1 - 1/p)) = 1 / (*/ (p-1)/p) = */ (p / (p-1))
+-- So, we just need to find the number less than the limit with the most distinct primes.
+-- Easy enough to do: generate and multiply together the primes, smallest to largest,
+-- until we hit the limit.
+
+solve :: Integer
+solve = last $ takeWhile (< 1000000) $ scanl (*) 1 primes
+
+--------------------------------------------------------------------------------
+
+-- Slow manual version.
 -- Runtime could probably be improved by doing the fold inline with the
 -- generation process. However, on my machine, solve runs in <3s; not bad.
 
-solve :: Integer
-solve = maxNOverPhi 1000000
-
---------------------------------------------------------------------------------
+manualSolve :: Integer
+manualSolve = maxNOverPhi 1000000
 
 phiWithIndices :: Word64 -> [(Integer, Word64)]
 phiWithIndices limit = zip [1 .. toInteger limit] $ phiST limit
