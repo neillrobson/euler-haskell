1accd8193c61aaeafb3112c5954789bf2f5eab65
Neill Robson <neill@neillrobson.com>
2026-04-17 10:31:03 -0400

    Fix inefficient phi instance imports

--- src/Utils/Primes.hs
+++ src/Utils/Primes.hs
@@ -3,6 +3,7 @@
 module Utils.Primes where
 
 import Control.Monad (forM_, when)
+import Data.Array.Base (UArray)
 import Data.Array.ST (modifyArray', newListArray, readArray, runSTUArray)
 import Data.Array.Unboxed (accum, elems, listArray)
 import Data.Int (Int64)
@@ -20,13 +21,14 @@ primes = 2 : 3 : filter isPrime (chain [5, 11 ..] [7, 13 ..])
 isPrime :: (Integral a) => a -> Bool
 isPrime x = all (\i -> x `mod` i /= 0) $ takeWhile (\i -> i * i <= x) primes
 
--- eulerPhi :: Int64 -> [Int64]
--- eulerPhi limit = elems $ accum (\x p -> x - x `div` p) phi primePairs
---   where
---     ps = takeWhile (<= limit) primes
---     idxs = map (\p -> takeWhile (<= limit) [p, p * 2 ..]) ps
---     primePairs = concat $ zipWith (\is p -> (,p) <$> is) idxs ps
---     phi = listArray (1, limit) [1 .. limit]
+eulerPhi :: Int64 -> [Int64]
+eulerPhi limit = elems $ accum (\x p -> x - x `div` p) phi primePairs
+  where
+    ps = takeWhile (<= limit) primes
+    idxs = map (\p -> takeWhile (<= limit) [p, p * 2 ..]) ps
+    primePairs = concat $ zipWith (\is p -> (,p) <$> is) idxs ps
+    phi :: UArray Int64 Int64
+    phi = listArray (1, limit) [1 .. limit]
 
 --------------------------------------------------------------------------------
 
