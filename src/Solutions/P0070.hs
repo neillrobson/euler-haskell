59b48a03d55a29775c7df07684a307d5b48b4960
Neill Robson <neill@neillrobson.com>
2026-04-20 13:04:04 -0400

    Prime powers won't solve p70

--- src/Solutions/P0070.hs
+++ src/Solutions/P0070.hs
@@ -11,13 +11,14 @@ import Utils.Primes (primes)
 -- with larger primes being better.
 -- The anagram condition also must hold.
 -- Any true prime has a phi of (p-1) which is definitely not an anagram.
--- So, we start with primes that remain below the limit when squared,
--- and check the anagram condition going down.
 
 solve :: Integer
-solve = (^ (2 :: Integer)) $ head $ filter (\p -> (p ^ (2 :: Integer)) `isAnagram` phiPrimeSq p) potentialPrimes
+solve = undefined
 
 --------------------------------------------------------------------------------
+
+limit :: Integer
+limit = 10000000
 
 isAnagram :: Integer -> Integer -> Bool
 isAnagram = go $ M.fromList $ map (,0 :: Integer) [0 .. 9]
@@ -32,9 +33,19 @@ isAnagram = go $ M.fromList $ map (,0 :: Integer) [0 .. 9]
               s' = M.adjust (subtract 1) bm $ M.adjust (+ 1) am s
            in go s' ad bd
 
-potentialPrimes :: [Integer]
-potentialPrimes = reverse $ takeWhile (<= floor (sqrt 10000000 :: Double)) primes
+-- So, we start with primes that remain below the limit when raised to a power,
+-- and check the anagram condition going down.
 
--- Return the totient value of the squared prime input.
-phiPrimeSq :: Integer -> Integer
-phiPrimeSq p = p ^ (2 :: Integer) - p
+potentialPrimes :: Integer -> [Integer]
+potentialPrimes pow = reverse $ takeWhile (<= floor ((** (1 / fromInteger pow)) $ fromInteger limit :: Double)) primes
+
+phiPrimePow :: Integer -> Integer -> (Integer, Integer)
+phiPrimePow pow pri = (curr, curr - prev)
+  where
+    prev = pri ^ (pow - 1)
+    curr = prev * pri
+
+-- However, `concatMap solutionsAt [2..23]` is empty.
+
+solutionsAt :: Integer -> [(Integer, Integer)]
+solutionsAt k = filter (uncurry isAnagram) $ map (phiPrimePow k) $ potentialPrimes k
