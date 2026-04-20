65410df1f51c58b6ac76f63a98daadd13935990a
Neill Robson <neill@neillrobson.com>
2026-04-20 12:28:19 -0400

    Just check prime squares, no solution yet

--- src/Solutions/P0070.hs
+++ src/Solutions/P0070.hs
@@ -3,11 +3,19 @@
 module Solutions.P0070 where
 
 import qualified Data.Map as M
-import Data.Word (Word64)
-import Solutions.P0069 (phiWithIndices)
+import Utils.Primes (primes)
+
+-- phi(n) = n * */ (1 - 1/p)
+-- n / phi(n) = 1 / (*/ (1 - 1/p)) = 1 / (*/ (p-1)/p) = */ (p / (p-1))
+-- This time, we need the number with the fewest distinct primes,
+-- with larger primes being better.
+-- The anagram condition also must hold.
+-- Any true prime has a phi of (p-1) which is definitely not an anagram.
+-- So, we start with primes that remain below the limit when squared,
+-- and check the anagram condition going down.
 
 solve :: Integer
-solve = undefined
+solve = (^ (2 :: Integer)) $ head $ filter (\p -> (p ^ (2 :: Integer)) `isAnagram` phiPrimeSq p) potentialPrimes
 
 --------------------------------------------------------------------------------
 
@@ -24,5 +32,9 @@ isAnagram = go $ M.fromList $ map (,0 :: Integer) [0 .. 9]
               s' = M.adjust (subtract 1) bm $ M.adjust (+ 1) am s
            in go s' ad bd
 
-filteredPhi :: Word64 -> [(Integer, Word64)]
-filteredPhi = filter (\(i, v) -> isAnagram i $ toInteger v) . phiWithIndices
+potentialPrimes :: [Integer]
+potentialPrimes = reverse $ takeWhile (<= floor (sqrt 10000000 :: Double)) primes
+
+-- Return the totient value of the squared prime input.
+phiPrimeSq :: Integer -> Integer
+phiPrimeSq p = p ^ (2 :: Integer) - p
