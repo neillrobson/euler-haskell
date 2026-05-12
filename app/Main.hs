282c76036ae12e99a103bc92da39554c780c6ffe
Neill Robson <neill@neillrobson.com>
2026-05-12 18:55:19 -0400

    Query problem from command line args

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@
 
 module Main where
 
+import Control.Monad ((>=>))
+import Data.Maybe (listToMaybe)
 import Solutions (buildDispatcher)
 import Solutions.P0048 (solve)
 import Solutions.P0069 (solve)
@@ -10,12 +12,13 @@ import Solutions.P0070 (solve)
 import Solutions.P0072 (solve)
 import Solutions.P0073 (solve)
 import Solutions.P0097 (solve)
+import System.Environment (getArgs)
 import System.IO (hFlush, stdout)
 import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
-  problem <- getInteger
+  problem <- getProblem
   let solution = answer problem
   case solution of
     Nothing -> putStrLn "Not yet solved"
@@ -36,6 +39,12 @@ getInteger = do
       putStrLn $ "Invalid integer " ++ line
       getInteger
     Just i -> return i
+
+getProblem :: IO Integer
+getProblem = do
+  args <- getArgs
+  let maybeProblem = (listToMaybe >=> readMaybe) args :: Maybe Integer
+  maybe getInteger return maybeProblem
 
 answer :: Integer -> Maybe Integer
 answer = $(buildDispatcher "src/Solutions")
