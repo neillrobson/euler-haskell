75b5eeb7497794745504f42744427ab331e93e3e
Neill Robson <neill@neillrobson.com>
2026-05-13 10:12:35 -0400

    Direct solution

--- src/Solutions/P0025.hs
+++ src/Solutions/P0025.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Solutions.P0025 where
 
 solve :: Integer
-solve = findFibWithDigitCount 1000
+solve = firstFibWithDigitCount 1000
 
 findFibWithDigitCount :: Integer -> Integer
 findFibWithDigitCount n = go 0 (1, 1)
@@ -12,3 +14,13 @@ digitCount :: Integer -> Integer
 digitCount = go 1
   where
     go i x = if x > 10 then go (i + 1) (x `div` 10) else i
+
+--------------------------------------------------------------------------------
+
+-- Direct computation using Binet's formula
+
+goldenRatio :: Double
+goldenRatio = (1 + sqrt 5) / 2
+
+firstFibWithDigitCount :: Integer -> Integer
+firstFibWithDigitCount (fromIntegral -> n) = ceiling $ (n - 1 + (logBase 10 5 / 2)) / logBase 10 goldenRatio
