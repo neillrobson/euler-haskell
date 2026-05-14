4fb43cc970fa6c9aa086164d3ad20f9942a62530
Neill Robson <neill@neillrobson.com>
2026-05-14 15:12:20 -0400

    Solve P28

--- src/Solutions/P0028.hs
+++ src/Solutions/P0028.hs
@@ -1,27 +1,12 @@
 module Solutions.P0028 where
 
 solve :: Integer
-solve = undefined
+solve = spiralDiagNLayers 500
 
-{-
-A scanning sum:
-1
-2 2 2 2
-4 4 4 4
-6 6 6 6
-
- 1
- 3  5  7  9
-13 17 21 25
-31 37 43 49
-
-1
-10 * 2 = 20; 20 + 4 * 1 = 24; 24 + 1 = 25
-10 * 4 = 40; 40 + 4 * 9 = 76; 76 + 25 = 101
--}
-
--- Without the initial 1^2 = 1
+-- Without the initial 1^2 = 1.
+-- Zero-indexed.
 firstNOddSquares :: Integer -> Integer
-firstNOddSquares n = 4 * n' ^ 2 + (11 * n' + 4 * n' ^ 3) `div` 3
-  where
-    n' = n - 1
+firstNOddSquares n = 4 * n ^ (2 :: Int) + (11 * n + 4 * n ^ (3 :: Int)) `div` 3
+
+spiralDiagNLayers :: Integer -> Integer
+spiralDiagNLayers n = 4 * firstNOddSquares n - 6 * n * (n + 1) + 1
