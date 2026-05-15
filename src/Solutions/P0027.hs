bf66e5e12e26aa8abb0643d4a24b1b23c1e2b44c
Neill Robson <neill@neillrobson.com>
2026-05-15 15:33:06 -0400

    wip P27, unbearably slow

---
+++ src/Solutions/P0027.hs
@@ -0,0 +1,20 @@
+module Solutions.P0027 where
+
+import Data.Foldable (maximumBy)
+import Data.Function (on)
+import Utils.Primes (isPrime)
+
+solve :: Integer
+solve = undefined
+
+pairs :: Int -> [(Int, Int)]
+pairs n = [(a, b) | a <- [negate n + 1 .. n - 1], b <- [negate n .. n]]
+
+quad :: (Int, Int) -> Int -> Int
+quad (a, b) n = n * n + a * n + b
+
+consecPrimes :: (Int, Int) -> Int
+consecPrimes ab = length $ takeWhile isPrime $ map (quad ab) [0 ..]
+
+longestConsecWithin :: Int -> (Int, Int)
+longestConsecWithin = maximumBy (compare `on` consecPrimes) . pairs
