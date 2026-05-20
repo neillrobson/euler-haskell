2a08b204ea1bbd4915d7e3b6046afdae5e00d4fc
Neill Robson <neill@neillrobson.com>
2026-05-20 15:58:19 -0400

    Problem 30 complete

--- src/Solutions/P0030.hs
+++ src/Solutions/P0030.hs
@@ -1,4 +1,21 @@
 module Solutions.P0030 where
 
 solve :: Integer
-solve = undefined
+solve = sum matchingFifthPowers
+
+-- | Produces digits in the opposite of reading order.
+digits :: Integer -> [Integer]
+digits 0 = []
+digits n = (n `mod` 10) : digits (n `div` 10)
+
+fifthPowerSum :: Integer -> Integer
+fifthPowerSum = sum . map (^ (5 :: Int)) . digits
+
+-- | The max fifth-power-sum of a seven digit number is only six digits,
+-- so we only need to check through six-digit numbers.
+maxToCheck :: Integer
+maxToCheck = 6 * 9 ^ (5 :: Int)
+
+-- | The number one is skipped since it isn't technically a sum.
+matchingFifthPowers :: [Integer]
+matchingFifthPowers = filter (\x -> x == fifthPowerSum x) [2 .. maxToCheck]
