ba3244c64c44ae1f152722595a2ffb8aac8d1e84
Neill Robson <neill@neillrobson.com>
2026-06-18 09:15:46 -0400

    Recursive multiplication, Integral typeclass

--- src/Utils/Powers.hs
+++ src/Utils/Powers.hs
@@ -8,3 +8,12 @@ bExp b e =
       e' = e `div` 2
       c = if odd e then b else 1
    in c * bExp b' e'
+
+-- | Recursive binary multiplication. O(lg n).
+bMul :: (Integral a) => a -> a -> a
+bMul _ 0 = 0
+bMul m n =
+  let m' = m + m
+      (n', r) = n `divMod` 2
+      c = if r == 1 then m else 0
+   in c + bMul m' n'
