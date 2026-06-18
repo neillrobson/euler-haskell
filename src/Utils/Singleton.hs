04d1bc62faf233fc51bce7ee54e7a8cd95c48afd
Neill Robson <neill@neillrobson.com>
2026-06-18 15:07:32 -0400

    wip learning about singletons

---
+++ src/Utils/Singleton.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+
+import Data.Kind (Type)
+
+-- Source: https://blog.jle.im/entry/introduction-to-singletons-1.html
+
+data DoorState = Opened | Closed | Locked deriving (Eq, Show)
+
+newtype Door (s :: DoorState) = UnsafeMkDoor {doorMaterial :: String}
+
+-- Equivalent with GADT
+data GDoor :: DoorState -> Type where
+  UnsafeMkGDoor :: {gDoorMaterial :: String} -> GDoor s
+
+-- Singleton version
+data SDoorState :: DoorState -> Type where
+  SOpened :: SDoorState 'Opened
+  SClosed :: SDoorState 'Closed
+  SLocked :: SDoorState 'Locked
