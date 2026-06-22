2e67eb18aa1a10d7c82d4e39260c6ec06fb61877
Neill Robson <neill@neillrobson.com>
2026-06-22 14:03:32 -0400

    Fill in boilerplate

--- src/Utils/Singleton2.hs
+++ src/Utils/Singleton2.hs
@@ -1,1 +1,18 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Utils.Singleton2 where
+
+import Data.Kind (Type)
+import Data.Singletons.TH (genSingletons)
+
+data DoorState = Opened | Closed | Locked deriving (Eq, Show)
+
+$(genSingletons [''DoorState])
+
+-- | Technically, @Door@ is three distinct types, indexed by a type of kind
+-- @DoorState@.
+data Door :: DoorState -> Type where
+  UnsafeMkDoor :: {doorMaterial :: String} -> Door s
