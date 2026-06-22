24461b89a83aaf7df60efd1dbf1c777e47df4342
Neill Robson <neill@neillrobson.com>
2026-06-22 09:42:49 -0400

    Implicit passing

--- src/Utils/Singleton.hs
+++ src/Utils/Singleton.hs
@@ -21,7 +21,7 @@ data SDoorState :: DoorState -> Type where
   SLocked :: SDoorState 'Locked
 
 --------------------------------------------------------------------------------
--- Usage examples
+-- Usage examples, explicit passing of singleton
 --------------------------------------------------------------------------------
 
 closeDoor :: Door 'Opened -> Door 'Closed
@@ -35,3 +35,33 @@ lockAnyDoor = \case
   SOpened -> lockDoor . closeDoor
   SClosed -> lockDoor
   SLocked -> id
+
+fromSDoorState :: SDoorState s -> DoorState
+fromSDoorState SOpened = Opened
+fromSDoorState SClosed = Closed
+fromSDoorState SLocked = Locked
+
+doorStatus :: SDoorState s -> Door s -> DoorState
+doorStatus state _ = fromSDoorState state
+
+--------------------------------------------------------------------------------
+-- Implicit singleton passing
+--------------------------------------------------------------------------------
+
+class SingDSI s where
+  singDS :: SDoorState s
+
+instance SingDSI 'Opened where
+  singDS = SOpened
+
+instance SingDSI 'Closed where
+  singDS = SClosed
+
+instance SingDSI 'Locked where
+  singDS = SLocked
+
+lockAnyDoor' :: (SingDSI s) => Door s -> Door 'Locked
+lockAnyDoor' = lockAnyDoor singDS
+
+doorStatus' :: (SingDSI s) => Door s -> DoorState
+doorStatus' = doorStatus singDS
