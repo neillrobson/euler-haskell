357fe4772c21bb6cf42b563a910deb029bfe3fda
Neill Robson <neill@neillrobson.com>
2026-06-30 07:49:37 -0400

    Scaffolding

--- src/Utils/Singleton3.hs
+++ src/Utils/Singleton3.hs
@@ -1,1 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Utils.Singleton3 where
+
+import Data.Kind (Type)
+import Data.Singletons.TH (Sing, genSingletons)
+
+data DoorState = Opened | Closed | Locked deriving (Eq, Show)
+
+$(genSingletons [''DoorState])
+
+type Door :: DoorState -> Type
+data Door s where
+  UnsafeMkDoor :: {doorMaterial :: String} -> Door s
+
+mkDoor :: Sing s -> String -> Door s
+mkDoor _ = UnsafeMkDoor
